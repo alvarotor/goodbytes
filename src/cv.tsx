dde147c384f14f448caafbb175af6ca53a462c12
Alvaro T <goodbytes23@gmail.com>
2025-05-13 13:19:27 +0200

    refactor: remove CV section from English and Spanish localization files

--- src/cv.tsx
+++ src/cv.tsx
@@ -9,25 +9,9 @@ export default function Cv() {
       <Header />
       <div class="wrap">
         <div id="content">
-          <h2>
-            <span class="red">{t("cv.title")}</span>
-          </h2>
-          <p>
-            {t("cv.downloadPrompt")}{" "}
-            <a href="content/Alvaro_Torres_CV.pdf" target="_blank">
-              Alvaro_Torres_CV.pdf
-            </a>
-          </p>
-          <p>
-            {t("cv.linkedinPrompt")}{" "}
-            <a
-              href="https://www.linkedin.com/in/alvarotorresp"
-              target="_blank"
-            >
-              {t("cv.linkedinLink")}
-            </a>
-            .
-          </p>
+
+
+
         </div>
       </div>
       <Footer />
